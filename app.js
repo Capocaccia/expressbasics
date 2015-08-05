676e388cbf5ea50eb7a31c4361fecf6431086a6a
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-08-05 09:46:47 -0500

    added new route

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var app = express();
 // var app = require express execute
 var routes = require('./routes/index');
 var pizza = require('./routes/pizza');
-var less = require('less-middleware')
+var cnug = require('./routes/cnug');
+var less = require('less-middleware');
+var morgan = require('morgan');
+var bp = require('body-parser');
+var fs = require('fs');
+var loggly = require('loggly');
+require('./lib/secrets');
 
 app.set('view engine', 'ejs');
 app.set('case sensitive routing', true);
@@ -11,18 +17,40 @@ app.set('strict routing', true);
 
 app.locals.title = "My Awesome App" ;
 
+// app.use(function(req, res, next){
+//   console.log('Request at ' + new Date().toISOString());
+//   next();
+// })
+// //handles a console log for requests at any url
+
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+//Creates a write stream and applies the "append a flag" which tells the file to append and not write
+
 app.use(function(req, res, next){
-  console.log('Request at ' + new Date().toISOString());
+  var client = require('./lib/loggly')('incoming');
+  client.log({ip: req.ip,
+   res: res, date: new Date(), url: req.url, method: req.method, status: res.statusCode, });
   next();
 })
-//handles a console log for requests at any url
+
+app.use(morgan('combined', {stream: logStream}))
+//combined is the style of log you wan
+//the second argument object tells morgan where to output those logs
+//these logs will not output to the console since we are streaming them into a file
+app.use(morgan('dev'))
+//morgan is an NPM package that handles server request logging.
+//dev is the style of log
+//these logs will print to a console since they are not being streamed to a file
 
 app.use(express.static('public'))
 //called a middleware
 //essentially creates a route for everything in the public folder
 
+app.use(bp.urlencoded({extended: false}))
+
 app.use('/', routes);
 app.use('/pizza', pizza);
+app.use('/cnug', cnug);
 
 app.use(less('public'));
 
